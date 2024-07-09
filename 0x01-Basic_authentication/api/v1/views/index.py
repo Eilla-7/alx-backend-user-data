4723f33a5eb52768ed0780b86d17932af5d58c1b
Eilla-7 <eilafadel2238@gmail.com>
2024-07-09 23:40:33 +0400

    forbidden() Edit

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -35,7 +35,7 @@ def unauthorized_() -> None:
     abort(401)
 
 
-@app_views.route('/Forbidden/', strict_slashes=False)
+@app_views.route('/forbidden/', strict_slashes=False)
 def forbidden() -> None:
     """GET /api/v1/Forbidden
     Return:
